b474692dbe95acdc33e53458815754a30f4d3f08
einhn <ainkm@naver.com>
2025-05-23 01:25:20 +0900

    fix: App.jsx 오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import MapPage from './pages/MapPage';
+import MapPage from './pages/MapPage'; // 상대 경로에 따라 조정 필요
 
-function App() {
+const App = () => {
   return (
     <div>
-      <h1>🚴 Path-Cast: Kakao Map 연동</h1>
+      <h1>🚴 Kakao Map 연동</h1>
       <MapPage />
     </div>
   );
-}
+};
+
+export default App;
